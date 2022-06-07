be9253277b8eddd8c35f2b1d147882c50ba5112f
Saint2999 <saint299999@gmail.com>
2022-06-07 21:52:35 +0300

    Добавил иерархию юнитов и базовые переменные

---
+++ Assets/Unit/Uniit_NPC/NPC_Behaviour.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NPC_Behaviour : Unit_Behaviour
+{
+    public float checkRadius;
+    public float attackRadius;
+    public bool shouldRotate;
+    public LayerMask whatIsPlayer;
+    protected Transform target;
+    protected bool isInChaseRange;
+    protected bool isInAttackRange;
+
+    private void Start()
+    {
+        
+    }
+
+
+    private void Update()
+    {
+    
+    }
+
+
+    protected void MoveCharacter(Vector2 dir)
+    {
+        rb.MovePosition((Vector2)transform.position + (dir * speed * Time.deltaTime));
+    }
+}
---
+++ Assets/Unit/Unit_Behaviour.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Unit_Behaviour : MonoBehaviour
+{
+    public float speed;
+    protected Animator anim;
+    protected Rigidbody2D rb;
+    protected Vector2 movement;
+    protected Vector3 dir;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
